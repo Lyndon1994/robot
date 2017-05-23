787c08ac8c2eac0fc2cdfee3141412f0db8eafad
Lyndon1994 <wuhulinyi@sina.com>
2017-05-23 11:29:42 +0800

    debug and ignore

--- remind.py
+++ remind.py
@@ -6,7 +6,7 @@ FORMAT = '%(asctime)-15s %(levelname)s:%(module)s:%(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 schedule.every(1).minutes.do(zhihu.ListenZhihu())
-schedule.every().day.at("6:00").do(wether.call())
+schedule.every().day.at("6:00").do(wether.call)
 while True:
     schedule.run_pending()
     time.sleep(1)
